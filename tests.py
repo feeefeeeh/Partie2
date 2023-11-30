ffd1b72fae07bd509f856dc340e1e3d8080d042b
felix <felixhumeau@yahoo.com>
2023-11-30 22:08:02 +0100

    v1

---
+++ tests.py
@@ -0,0 +1,12 @@
+# tests.py
+
+def test_addition():
+    assert 1 + 1 == 2, "Erreur d'addition"
+
+def test_multiplication():
+    assert 2 * 3 == 6, "Erreur de multiplication"
+
+def test_division():
+    assert 6 / 2 == 3, "Erreur de division"
+
+# Ajoutez d'autres tests en fonction de vos besoins
