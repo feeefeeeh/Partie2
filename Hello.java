34fd80e96e0c11ba30a4163c012c41a50bbbd161
feeefeeeh <115033400+feeefeeeh@users.noreply.github.com>
2023-12-02 19:18:21 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ECE, c'est billel !!");
+        System.out.println("Hello ECE, c'est felix !!");
     }
 }
 
